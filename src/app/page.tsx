967b0d67399d81c6b7aaaacd01b14a726ccd981b
fabiobrl <62523484+fabiobrl@users.noreply.github.com>
2025-01-20 10:38:11 -0300

    Create page.tsx

---
+++ src/app/page.tsx
@@ -0,0 +1,12 @@
+'use client'
+
+import React from 'react';
+import FormularioCinematografico from './components/FormularioCinematografico';
+
+export default function Home() {
+  return (
+    <main>
+      <FormularioCinematografico />
+    </main>
+  );
+}
